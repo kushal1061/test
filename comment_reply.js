8c1797c5629803b04cd82e31851b0aa4876047e1
kushal <kushalpalbishnoi@gmail.com>
2025-11-08 22:10:42 +0530

    trying reply 9

--- comment_reply.js
+++ comment_reply.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const HOST_URL = "graph.instagram.com";
 const IG_COMMENT_ID = "<IG_COMMENT_ID>";
 const ACCESS_TOKEN = "IGAASedoy93TZABZAFN0QnlpZAFhlSU5LaFE4Q3dTNXM2U2g2Rm1FUWtyUmdFX3oyZAUFyUURVWFl5UVpwTTJLWG5ZAQ05YYW1hSnlPMW1ZAazEzVmFMdmRWMUIxdTdVaVA2XzdfM1A4SGlhYjc1TXpBQ2drb2pPMXRjYkJQNllVSFFDRQZDZD"; // if needed for authentication
-async function replyToComment(CommentId,userId) {
+async function replyToComment(commentId,userId) {
   const url = `https://${HOST_URL}/v24.0/${userId}/messages`;
   try {
     const response = await axios.post(
