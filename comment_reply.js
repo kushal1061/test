eb55e9ed109de69786c117095773b560c3684a49
kushal <kushalpalbishnoi@gmail.com>
2025-11-09 11:19:24 +0530

    trying reply jyoti 4

--- comment_reply.js
+++ comment_reply.js
@@ -6,14 +6,24 @@ const ACCESS_TOKEN = "IGAASedoy93TZABZAFRhU3ZAtM2xIQXpQUVRUeTdKQTA1a3JTOW92OWluU
 // const              IGAASedoy93TZABZAFRhU3ZAtM2xIQXpQUVRUeTdKQTA1a3JTOW92OWluU3RCUjd1OTRwMjZA6T0dkYThwRkQ4ODdDck1GZA0F1SFZAPaWp2aVZAnSGhLS19maF9OWWptZAjJNLVowOE1HQ2JUbXhEcjR5Y1J4NDN6LVdIazMyY0F4UnJmSQZDZD// if needed for authentication
 // const userId = "17841477915554405";
 const userId ="17841449485648739";
-async function replyToComment(commentId) {
+async function sendMessageCommentReply(commentId) {
   const url = `https://${HOST_URL}/v24.0/${userId}/messages`;
   try {
     const response = await axios.post(
       url,
       {
         recipient: { comment_id: commentId },
-        message: { text: "hello" }
+        message: { text: "Hey ✈️ Thanks for reaching out! What do you want to explore?",
+    "quick_replies": [
+      {
+        "title": "Flight Offers",
+        "payload": "FLIGHT_OFFERS"
+      },
+      {
+        "title": "Talk to Support",
+        "payload": "TALK_SUPPORT"
+      }
+    ] }
       },
       {
         headers: {
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.json());
-const token = "kushalpal"
+const token = "kushalpal";
 app.get('/', (req, res) => {
    console.log(req.query);
   if (
